8c467ae085e33715e872c3963a327e03f215a30e
James Jacobsen <edjcjacobsen@gmail.com>
2023-02-10 01:03:08 -0600

    Added isFull definition and tests

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,34 +2,57 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+// Global variables //
 let basket = [];
+const MAX_ITEMS = 5;
 
-// addItem Test //
+// Tests //
+// addItem  
 addItem('Broccoli');
 addItem('Potatoes');
 console.log(basket);
 
-// listItem Test //
+// listItem 
 listItems();
 
-// empty Test //
+// empty 
 empty();
 
+// isFull - false
+console.log(`Expected result is false: ${isFull()}`);
+basket = ['Chicken', 'Beans', 'Ice Cream'];
+console.log(`Expected result is false: ${isFull()}`);
+
+basket.push('Avocado');
+basket.push('Eggs');
+
+//isFull - true
+console.log(`Expected result is true: ${isFull()}`);
+basket.push('Bread');
+console.log(`Expected result is true: ${isFull()}`);
+
+//End Tests //
+
+
 // Function Definitions //
-function addItem(item){ // Adds an item to the basket array
+
+// Adds an item to the basket array
+function addItem(item){ 
     basket.push(item);
     console.count(`Added ${item} into the basket`);
     return true;
 }
 
-function listItems(){ // Logs every item in basket to the console
+// Logs every item in basket to the console
+function listItems(){ 
     console.log('*Item List*\n')
     for(let item of basket){
         console.log('>', item);
     }
 }
 
-function empty(){ // Empties all items from the list
+// Empties all items from the list
+function empty(){ 
     for(let i = basket.length - 1; i >= 0; i--){
         console.log(`${basket.pop(i)} removed from basket`);
     }
@@ -40,5 +63,13 @@ function empty(){ // Empties all items from the list
     else{
         console.log('Basket is now empty');
     }
+}
+
+// Checks if the basket is full
+function isFull(){
+    if(basket.length < MAX_ITEMS){
+        return false;
+    }
+    return true;
 }
 // End Function Definitions //
