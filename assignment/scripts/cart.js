123c503fb145829fbafe459f52db4482be4ea6e2
James Jacobsen <edjcjacobsen@gmail.com>
2023-02-10 00:19:44 -0600

    Added basket array and addItem function

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,15 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+let basket = [];
+addItem('Broccoli');
+addItem('Potatoes');
+
+console.log(basket);
+
+function addItem(item){
+    basket.push(item);
+    console.count(`Added ${item} into the basket`);
+    return true;
+}
