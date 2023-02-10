bc19cd46b61e9132efc5fde32b19429ab48171db
James Jacobsen <edjcjacobsen@gmail.com>
2023-02-10 00:44:28 -0600

    Added function empty definition and test

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -12,6 +12,9 @@ console.log(basket);
 // listItem Test //
 listItems();
 
+// empty Test //
+empty();
+
 // Function Definitions //
 function addItem(item){ // Adds an item to the basket array
     basket.push(item);
@@ -23,6 +26,19 @@ function listItems(){ // Logs every item in basket to the console
     console.log('*Item List*\n')
     for(let item of basket){
         console.log('>', item);
+    }
+}
+
+function empty(){ // Empties all items from the list
+    for(let i = basket.length - 1; i >= 0; i--){
+        console.log(`${basket.pop(i)} removed from basket`);
+    }
+    if(basket.length != 0){
+        console.log(`Error: Basket has ${basket.length} item(s) remaining`);
+        console.log(basket);
+    }
+    else{
+        console.log('Basket is now empty');
     }
 }
 // End Function Definitions //
