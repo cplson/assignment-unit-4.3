222aa67f902a5405713d1deb7eb93320fb156f36
James Jacobsen <edjcjacobsen@gmail.com>
2023-02-10 00:29:08 -0600

    Added listItems definition and test

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -3,13 +3,26 @@ console.log('***** Cart Functions *****');
 // We want to see how you are testing your code!!!
 
 let basket = [];
+
+// addItem Test //
 addItem('Broccoli');
 addItem('Potatoes');
-
 console.log(basket);
 
-function addItem(item){
+// listItem Test //
+listItems();
+
+// Function Definitions //
+function addItem(item){ // Adds an item to the basket array
     basket.push(item);
     console.count(`Added ${item} into the basket`);
     return true;
 }
+
+function listItems(){ // Logs every item in basket to the console
+    console.log('*Item List*\n')
+    for(let item of basket){
+        console.log('>', item);
+    }
+}
+// End Function Definitions //
